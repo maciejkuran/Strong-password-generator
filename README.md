3a0ae719b83ceabdcd7b5278624f31560b2377a9
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-10-27 14:38:21 +0200

    commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Super strong password generator –npm
+
+Get a strong, unique, and secure password immediately.
+
+# Installation
+
+This library can be exported as Node.js modules or as ES modules.
+`npm i super-strong-password-generator --save`
+`npm i super-strong-password-generator-es –save`
+
+\*To save as `devDependencies` use `--save-dev`
+
+##### Node.js modules
+
+```
+Const pw = require(‘super-strong-password-generator’);
+//then just call the function
+pw. caseSensitive_numbs_PW(15)
+//Using @Parcel bundler
+import { caseSensitive_numbs_PW) from ‘super-strong-password-generator’
+//then just call the function
+caseSensitive_numbs_PW(15)
+```
+
+##### ES modules
+
+```
+import {caseSensitive_numbs_PW} from ‘./node_modules/super-strong-password-generator/pw-generator.js;
+//then just call the function
+caseSensitive_numbs_PW(15)
+```
+
+## Options
+
+The function takes one argument – the length of the password.
+There are multiple options for password generation.
+
+- `lowercase_PW` - generates lowercase random letters
+- `caseSensitive_PW` - generates case sensitive random letters
+- `caseSensitive_numbs_PW` - generates case sensitive letters + numbers
+- `caseSensitive_SpecialCharacters_PW` - generates case sensitive letters + special characters
+- `caseSensitive_Numbs_SpecialCharacters_PW` - generates case sensitive letters + numbers + special characters (recommended option due security reasons)
+
+## Issues
+
+Report any inconveniences if they are noticed. Thank you in advance.
---
+++ nodeJS/pw-generator.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const alpha = Array.from({ length: 26 }, (val = 65, i) => val + i);
+const alphabetLowercase = alpha.map((x) => String.fromCharCode(x)).map((x) => x.toLowerCase());
+
+//prettier-ignore
+const specialCharacters = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '>', '<', '(', ')', '{', '}', '[', ']'];
+
+const randomNb = (max) => {
+  return Math.trunc(Math.random() * max);
+};
+
+const randomNumbs = (pw) => {
+  let numbers;
+
+  if (pw.length <= 8) {
+    numbers = [randomNb(pw.length), randomNb(pw.length)];
+  }
+
+  if (pw.length > 8) {
+    numbers = [randomNb(pw.length), randomNb(pw.length), randomNb(pw.length), randomNb(pw.length)];
+  }
+
+  if (pw.length > 16) {
+    numbers = [
+      randomNb(pw.length),
+      randomNb(pw.length),
+      randomNb(pw.length),
+      randomNb(pw.length),
+      randomNb(pw.length),
+      randomNb(pw.length),
+    ];
+  }
+
+  return numbers;
+};
+
+//____________________________________________
+//1) Output = string: lowerCased password, simply provide the password length as argument
+const output = [];
+
+const lowercase_PW = (length) => {
+  let index = randomNb(alphabetLowercase.length);
+
+  if (output.length === length) return output.join("");
+
+  output.push(alphabetLowercase[index]);
+
+  return lowercase_PW(length);
+};
+
+//____________________________________________
+//1) Output = string: lowercased password, simply provide the password length as argument
+const caseSensitive_PW = (length) => {
+  const upperLetters = lowercase_PW(length).split("");
+  const indexes = randomNumbs(upperLetters);
+
+  indexes.forEach((i) => upperLetters.splice(i, 1, upperLetters[i].toUpperCase()));
+
+  return upperLetters.join("");
+};
+
+//____________________________________________
+//2) Output = string: Case-sensitive password, simply provide the password length as argument
+const caseSensitive_numbs_PW = (length) => {
+  const caseSensitive = caseSensitive_PW(length).split("");
+  const indexes = randomNumbs(caseSensitive);
+
+  indexes.forEach((i) => caseSensitive.splice(i, 1, String(randomNb(11))));
+
+  return caseSensitive.join("");
+};
+
+//____________________________________________
+//3) Output = string: Case-sensitive with special characters password, simply provide the password length as argument
+const caseSensitive_SpecialCharacters_PW = (length) => {
+  const caseSensitiveNumbs = caseSensitive_PW(length).split("");
+  const indexes = randomNumbs(caseSensitiveNumbs);
+
+  indexes.forEach((i) => caseSensitiveNumbs.splice(i, 1, specialCharacters[i]));
+
+  return caseSensitiveNumbs.join("");
+};
+
+//____________________________________________
+//4) Output = string: Case-sensitive with numbers and special characters password, simply provide the password length as argument
+const caseSensitive_Numbs_SpecialCharacters_PW = (length) => {
+  const caseSensitiveNumbs = caseSensitive_numbs_PW(length).split("");
+  const indexes = randomNumbs(caseSensitiveNumbs);
+
+  indexes.forEach((i) => caseSensitiveNumbs.splice(i, 1, specialCharacters[i]));
+
+  return caseSensitiveNumbs.join("");
+};
+
+module.exports = {
+  lowercase_PW: lowercase_PW,
+  caseSensitive_PW: caseSensitive_PW,
+  caseSensitive_numbs_PW: caseSensitive_numbs_PW,
+  caseSensitive_SpecialCharacters_PW: caseSensitive_SpecialCharacters_PW,
+  caseSensitive_Numbs_SpecialCharacters_PW: caseSensitive_Numbs_SpecialCharacters_PW,
+};
